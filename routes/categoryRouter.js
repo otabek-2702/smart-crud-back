0fa815752378f137fb9bd657ceaa995fcdd100a1
otabek-2702 <adrian3806880@gmail.com>
2024-07-27 18:42:27 +0500

    permission to seller getting categories

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -7,10 +7,11 @@ const {
   deleteCategory
 } = require("../controller/categoryCtrl");
 const isAdmin = require("../middlewares/isAdmin");
+const isSeller = require("../middlewares/isSeller");
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", isAdmin, createCategory);
-categoryRouter.get("/", isAdmin, getCategories);
+categoryRouter.get("/", [isAdmin, isSeller], getCategories);
 categoryRouter.get("/:categoryID", isAdmin, getCategoryById);
 categoryRouter.put("/:categoryID", isAdmin, updateCategory);
 categoryRouter.delete("/:categoryID", isAdmin, deleteCategory);
